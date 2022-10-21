946077266ec7a80cfa4d22479e42ae281d1a6abb
Katy Senia <katy.develops@gmail.com>
2022-10-21 01:05:41 -0400

    Added Local Notifications feature

--- screens/ReservationScreen.js
+++ screens/ReservationScreen.js
@@ -11,14 +11,13 @@ import {
 import { Picker } from "@react-native-picker/picker";
 import DateTimePicker from "@react-native-community/datetimepicker";
 import * as Animatable from "react-native-animatable";
-import { SearchBar } from "react-native-elements";
+import * as Notifications from "expo-notifications";
 
 const ReservationScreen = () => {
 	const [campers, setCampers] = useState(1);
 	const [hikeIn, setHikeIn] = useState(false);
 	const [date, setDate] = useState(new Date());
 	const [showCalendar, setShowCalendar] = useState(false);
-	//const [showModal, setShowModal] = useState(false);
 
 	const onDateChange = (event, selectedDate) => {
 		const currentDate = selectedDate || date;
@@ -28,29 +27,35 @@ const ReservationScreen = () => {
 
 	const handleReservation = () => {
 		const message = `Number of Campers: ${campers}
-                    \nHike-In? ${hikeIn}
-                    \nDate: ${date.toLocaleDateString("en-US")}`;
-
+                            \nHike-In? ${hikeIn}
+                            \nDate: ${date.toLocaleDateString("en-US")}`;
 		Alert.alert(
 			"Begin Search?",
 			message,
 			[
-				({
+				{
 					text: "Cancel",
-					onPress: resetForm,
+					onPress: () => {
+						console.log("Reservation Search Canceled");
+						resetForm();
+					},
+					style: "cancel",
 				},
 				{
-					text: "Ok",
-					onPress: resetForm,
-				}),
+					text: "OK",
+					onPress: () => {
+						presentLocalNotification(
+							date.toLocaleDateString("en-US")
+						);
+						resetForm();
+					},
+				},
 			],
 			{ cancelable: false }
 		);
-
 		console.log("campers:", campers);
 		console.log("hikeIn:", hikeIn);
 		console.log("date:", date);
-		//setShowModal(!showModal);
 	};
 
 	const resetForm = () => {
@@ -58,6 +63,34 @@ const ReservationScreen = () => {
 		setHikeIn(false);
 		setDate(new Date());
 		setShowCalendar(false);
+	};
+
+	const presentLocalNotification = async (reservationDate) => {
+		const sendNotification = () => {
+			Notifications.setNotificationHandler({
+				handleNotification: async () => ({
+					shouldShowAlert: true,
+					shouldPlaySound: true,
+					shouldSetBadge: true,
+				}),
+			});
+
+			Notifications.scheduleNotificationAsync({
+				content: {
+					title: "Your Campsite Reservation Search",
+					body: `Search for ${reservationDate} requested`,
+				},
+				trigger: null,
+			});
+		};
+
+		let permissions = await Notifications.getPermissionsAsync();
+		if (!permissions.granted) {
+			permissions = await Notifications.requestPermissionsAsync();
+		}
+		if (permissions.granted) {
+			sendNotification();
+		}
 	};
 
 	return (
@@ -113,40 +146,11 @@ const ReservationScreen = () => {
 						accessibilityLabel="Tap me to search for available campsites to reserve"
 					/>
 				</View>
-
-				{/* <Modal
-					animationType="slide"
-					transparent={false}
-					visible={showModal}
-					onRequestClose={() => setShowModal(!showModal)}
-				>
-					<View style={styles.modal}>
-						<Text style={styles.modalTitle}>
-							Search Campsite Reservations
-						</Text>
-						<Text style={styles.modalText}>
-							Number of Campers: {campers}
-						</Text>
-						<Text style={styles.modalText}>
-							Hike-In?: {hikeIn ? "Yes" : "No"}
-						</Text>
-						<Text style={styles.modalText}>
-							Date: {date.toLocaleDateString("en-US")}
-						</Text>
-						<Button
-							onPress={() => {
-								setShowModal(!showModal);
-								resetForm();
-							}}
-							color="#5637DD"
-							title="Close"
-						/>
-					</View>
-				</Modal> */}
 			</Animatable.View>
 		</ScrollView>
 	);
 };
+
 const styles = StyleSheet.create({
 	formRow: {
 		alignItems: "center",
@@ -162,25 +166,6 @@ const styles = StyleSheet.create({
 	formItem: {
 		flex: 1,
 	},
-	alertHeader: {
-		fontSize: 18,
-	},
-	// modal: {
-	// 	justifyContent: "center",
-	// 	margin: 20,
-	// },
-	// modalTitle: {
-	// 	fontSize: 24,
-	// 	fontWeight: "bold",
-	// 	backgroundColor: "#5637DD",
-	// 	textAlign: "center",
-	// 	color: "#fff",
-	// 	marginBottom: 20,
-	// },
-	// modalText: {
-	// 	fontSize: 18,
-	// 	margin: 10,
-	// },
 });
 
 export default ReservationScreen;
